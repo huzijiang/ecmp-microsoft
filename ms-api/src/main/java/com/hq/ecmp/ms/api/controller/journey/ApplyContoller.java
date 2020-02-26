c71720cbddc5b18b61c3fc9df8497f4cdbda2e9f
Cairuikang <18335106425@163.com>
2020-02-26 15:38:08 +0800

    订单生成服务

--- ms-api/src/main/java/com/hq/ecmp/ms/api/controller/journey/ApplyContoller.java
+++ ms-api/src/main/java/com/hq/ecmp/ms/api/controller/journey/ApplyContoller.java
@@ -1,20 +1,26 @@
 package com.hq.ecmp.ms.api.controller.journey;
 
+import com.github.pagehelper.PageHelper;
 import com.hq.common.core.api.ApiResponse;
+import com.hq.common.utils.DateUtils;
 import com.hq.ecmp.ms.api.dto.base.RegimeDto;
 import com.hq.ecmp.ms.api.dto.base.UserDto;
 import com.hq.ecmp.ms.api.dto.journey.JourneyApplyDto;
 import com.hq.ecmp.mscore.domain.ApplyInfo;
-import com.hq.ecmp.mscore.dto.ApplyTravelRequest;
-import com.hq.ecmp.mscore.dto.JourneyCommitApplyDto;
+import com.hq.ecmp.mscore.domain.JourneyInfo;
+import com.hq.ecmp.mscore.domain.OrderInfo;
 import com.hq.ecmp.mscore.service.IApplyInfoService;
+import com.hq.ecmp.mscore.service.IJourneyInfoService;
+import com.hq.ecmp.mscore.service.IOrderInfoService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.hq.ecmp.mscore.dto.ApplyOfficialRequest;
-
 
 import java.util.List;
 
@@ -29,47 +35,23 @@ public class ApplyContoller {
     @Autowired
     private IApplyInfoService applyInfoService;
 
+    @Autowired
+    private IJourneyInfoService journeyInfoService;
+
+    @Autowired
+    private IOrderInfoService orderInfoService;
+
     /**
-     * 员工提交行程申请     弃用。公务跟差旅申请分两个接口实现
-     * @param  journeyCommitApplyDto  行程申请信息
-     * @param
+     * 员工提交行程申请
+     * @param  journeyApplyDto  行程申请信息
+     * @param  userDto    申请用户信息
      * @return
      */
-    @Deprecated()
     @ApiOperation(value = "applyCommit",notes = "员工提交行程申请，行程信息必须全面 ",httpMethod ="POST")
     @PostMapping("/applyCommit")
-    public ApiResponse   applyCommit(JourneyCommitApplyDto journeyCommitApplyDto){
-        //提交行程申请
-        applyInfoService.applyCommit(journeyCommitApplyDto);
-        return ApiResponse.success();
-    }
+    public ApiResponse   applyCommit(JourneyApplyDto journeyApplyDto, UserDto userDto){
 
-    /**
-     * 员工提交公务行程申请
-     * @param  officialCommitApply  行程申请信息
-     * @param
-     * @return
-     */
-    @ApiOperation(value = "applyOfficialCommit",notes = "员工提交行程申请，行程信息必须全面 ",httpMethod ="POST")
-    @PostMapping("/applyOfficialCommit")
-    public ApiResponse   applyOfficialCommit(ApplyOfficialRequest officialCommitApply){
-        //提交公务行程申请
-        applyInfoService.applyOfficialCommit(officialCommitApply);
-        return ApiResponse.success();
-    }
-
-    /**
-     * 员工提交差旅行程申请
-     * @param  travelCommitApply  行程申请信息
-     * @param
-     * @return
-     */
-    @ApiOperation(value = "applyOfficialCommit",notes = "员工提交行程申请，行程信息必须全面 ",httpMethod ="POST")
-    @PostMapping("/applyOfficialCommit")
-    public ApiResponse   applyTravelCommit(ApplyTravelRequest travelCommitApply){
-        //提交差旅行程申请
-        applyInfoService.applytravliCommit(travelCommitApply);
-        return ApiResponse.success();
+        return null;
     }
 
     /**
@@ -103,9 +85,11 @@ public class ApplyContoller {
      */
     @ApiOperation(value = "getWaitApproveApplies",notes = "获取等待当前用户审批的行程申请列表 ",httpMethod ="POST")
     @PostMapping("/getWaitApproveApplies")
-    public ApiResponse<List<ApplyInfo>>   getWaitApproveApplies(UserDto userDto){
-
-        return null;
+    public ApiResponse   getWaitApproveApplies(UserDto userDto){
+        JourneyInfo journeyInfo = JourneyInfo.builder().userId(userDto.getUserId()).build();
+        // PageHelper.startPage(userDto.get)
+        List<JourneyInfo> journeyInfos = journeyInfoService.selectJourneyInfoList(journeyInfo);
+        return ApiResponse.success("查询单前用户可审批行程单列表成功",journeyInfos);
     }
 
     /**
@@ -140,7 +124,7 @@ public class ApplyContoller {
      */
     @ApiOperation(value = "getPassengerOwnerAppliesDetailInfo",notes = "获取乘客自身的行程申请详细信息 ",httpMethod ="POST")
     @PostMapping("/getPassengerOwnerAppliesDetailInfo")
-    public ApiResponse   getPassengerOwnerAppliesDetailInfo(JourneyCommitApplyDto journeyApplyDto){
+    public ApiResponse   getPassengerOwnerAppliesDetailInfo(JourneyApplyDto journeyApplyDto){
 
         return null;
     }
@@ -153,9 +137,28 @@ public class ApplyContoller {
      */
     @ApiOperation(value = "applyPass",notes = "行程申请-审核通过 ",httpMethod ="POST")
     @PostMapping("/applyPass")
-    public ApiResponse   applyPass(JourneyCommitApplyDto journeyApplyDto, UserDto userDto){
-
-        return null;
+    @Transactional
+    public ApiResponse applyPass(JourneyApplyDto journeyApplyDto,UserDto userDto){
+        //1.校验信息
+        JourneyInfo journeyInfo = journeyInfoService.selectJourneyInfoById(journeyApplyDto.getJouneyId());
+        if (ObjectUtils.isEmpty(journeyInfo)){
+            return ApiResponse.error("行程申请单不存在!");
+        }
+        ApplyInfo applyInfo = ApplyInfo.builder().journeyId(journeyApplyDto.getJouneyId()).build();
+        List<ApplyInfo> applyInfos = applyInfoService.selectApplyInfoList(applyInfo);
+        if (CollectionUtils.isNotEmpty(applyInfos)){
+            return ApiResponse.error("行程申请单已审批！");
+        }
+        //2.生成行程单审批信息
+        applyInfo = ApplyInfo.builder().applyId(Long.MAX_VALUE).applyType("").approverName("").journeyId(journeyInfo.getJourneyId()).costCenter(userDto.getUserId()).projectId(userDto.getUserId()).
+                approverName("").reason("").state("").regimenId(Long.MAX_VALUE).build();
+        applyInfo.setCreateBy(userDto.getUserName());
+        applyInfo.setCreateTime(DateUtils.getNowDate());
+        applyInfoService.insertApplyInfo(applyInfo);
+        //3.默认生成订单
+        OrderInfo orderInfo = OrderInfo.builder().orderId(Long.MAX_VALUE).journeyId(journeyInfo.getJourneyId()).build();
+        orderInfoService.insertOrderInfo(orderInfo);
+        return ApiResponse.success("行程申请单审批通过");
     }
 
 
@@ -167,7 +170,7 @@ public class ApplyContoller {
      */
     @ApiOperation(value = "applyReject",notes = "行程申请-驳回",httpMethod ="POST")
     @PostMapping("/applyReject")
-    public ApiResponse   applyReject(JourneyCommitApplyDto journeyApplyDto, UserDto userDto){
+    public ApiResponse   applyReject(JourneyApplyDto journeyApplyDto,UserDto userDto){
 
         return null;
     }
@@ -180,9 +183,9 @@ public class ApplyContoller {
      */
     @ApiOperation(value = "getApplyApproveDetailInfo",notes = "获取行程申请的审批详情",httpMethod ="POST")
     @PostMapping("/getApplyApproveDetailInfo")
-    public ApiResponse   getApplyApproveDetailInfo(JourneyCommitApplyDto journeyApplyDto){
-
-        return null;
+    public ApiResponse   getApplyApproveDetailInfo(JourneyApplyDto journeyApplyDto){
+        JourneyInfo journeyInfo = journeyInfoService.selectJourneyInfoById(journeyApplyDto.getJouneyId());
+        return ApiResponse.success("查询申请单详情成功",journeyInfo);
     }
 
 }
--- ms-core/src/main/java/com/hq/ecmp/mscore/domain/ApplyInfo.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/domain/ApplyInfo.java
@@ -1,6 +1,8 @@
 package com.hq.ecmp.mscore.domain;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.hq.core.aspectj.lang.annotation.Excel;
@@ -13,6 +15,8 @@ import com.hq.core.web.domain.BaseEntity;
  * @date 2020-01-02
  */
 @Data
+@Builder
+@NoArgsConstructor
 public class ApplyInfo extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
