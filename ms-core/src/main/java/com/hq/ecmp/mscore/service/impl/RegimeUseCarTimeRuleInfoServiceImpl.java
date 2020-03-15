3f0b4f7bd65bd3c292f59a1f2fd7b1e75bfb581b
chenmi <1522472057@qq.com>
2020-03-15 20:45:49 +0800

    新增公务制度用车时段生成规则优化&制度详情

---
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/RegimeUseCarTimeRuleInfoServiceImpl.java
@@ -0,0 +1,30 @@
+package com.hq.ecmp.mscore.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hq.ecmp.mscore.domain.RegimeUseCarTimeRuleInfo;
+import com.hq.ecmp.mscore.mapper.RegimeUseCarTimeRuleInfoMapper;
+import com.hq.ecmp.mscore.service.IRegimeUseCarTimeRuleInfoService;
+
+
+
+@Service
+public class RegimeUseCarTimeRuleInfoServiceImpl implements IRegimeUseCarTimeRuleInfoService {
+	@Autowired
+	RegimeUseCarTimeRuleInfoMapper regimeUseCarTimeRuleInfoMapper;
+
+	@Override
+	public Integer batchInsert(List<RegimeUseCarTimeRuleInfo> list) {
+		return regimeUseCarTimeRuleInfoMapper.batchInsert(list);
+	}
+
+	
+	
+	
+	
+	
+
+}
