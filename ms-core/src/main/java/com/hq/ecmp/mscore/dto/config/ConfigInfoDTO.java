12461c23894609192997254461d27cf71f5c3ff8
xueyong <xueyong@01zhuanche.com>
2020-03-16 18:21:14 +0800

    增加企业设置成功

---
+++ ms-core/src/main/java/com/hq/ecmp/mscore/dto/config/ConfigInfoDTO.java
@@ -0,0 +1,46 @@
+package com.hq.ecmp.mscore.dto.config;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author xueyong
+ * @date 2020/3/16
+ * ecmp-microservice.
+ */
+@Data
+public class ConfigInfoDTO {
+
+    /**
+     * 基本信息配置
+     */
+    @ApiModelProperty(name = "baseInfo", value = "基本信息配置")
+    private String baseInfo;
+
+    /**
+     * 背景图配置
+     */
+    @ApiModelProperty(name = "backgroundImageInfo", value = "背景图配置")
+    private String backgroundImageInfo;
+
+    @ApiModelProperty(name = "messageInfo", value = "公告配置")
+    private String messageInfo;
+
+    @ApiModelProperty(name = "welcomeImageInfo", value = "开屏图配置")
+    private String welcomeImageInfo;
+
+    @ApiModelProperty(name = "smsInfo", value = "短信配置")
+    private String smsInfo;
+
+    @ApiModelProperty(name = "virtualPhoneInfo", value = "虚拟小号配置")
+    private String virtualPhoneInfo;
+
+    @ApiModelProperty(name = "orderConfirmInfo", value = "确认订单方式配置")
+    private String orderConfirmInfo;
+
+    @ApiModelProperty(name = "dispatchInfo", value = "自动派单方式配置")
+    private String dispatchInfo;
+
+    @ApiModelProperty(name = "waitMaxMinute", value = "往返用车等待时长配置")
+    private String waitMaxMinute;
+}
