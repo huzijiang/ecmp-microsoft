05622958f7a9ca8cc8aaf6ba967eb94f69ec9232
ZhangChao <zhangsan@itcast.cn>
2020-03-17 22:54:59 +0800

    车型查询/排序

---
+++ ms-core/src/main/java/com/hq/ecmp/mscore/dto/CarTypeSortDTO.java
@@ -0,0 +1,14 @@
+package com.hq.ecmp.mscore.dto;
+
+import lombok.Data;
+
+/**
+ * @Author: chao.zhang
+ * @Date: 2020/3/17 9:15
+ */
+@Data
+public class CarTypeSortDTO {
+
+    private Long mainCarTypeId;
+    private Long targetCarTypeId;
+}
