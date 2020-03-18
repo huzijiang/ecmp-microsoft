44706516604783c505be298794cd476c971463b3
huzj <huzijiang@hqzhuanche.com>
2020-03-19 00:36:13 +0800

    调度选车选人 初始化

--- ms-core/src/main/java/com/hq/ecmp/mscore/bo/DispatcherOwnedContainer.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/bo/DispatcherOwnedContainer.java
@@ -1,28 +1,34 @@
 package com.hq.ecmp.mscore.bo;
 
+import lombok.Data;
+
 import java.util.LinkedHashMap;
 import java.util.concurrent.PriorityBlockingQueue;
 
 /**
- * 调度员 操作容器类  用于存储
  *
- *
+ * 调度员 操作容器类  用于临时存储待调度的车  和  驾驶员
  *
  * @Author: zj.hu
  * @Date: 2020-03-17 21:38
  */
+@Data
 public class DispatcherOwnedContainer {
 
     /**
      *
      * key: String= 订单-调度员
-     *
+     * PriorityBlockingQueue<WaitSelectedCarBo>  待调度的车辆
      *
      */
-    public static LinkedHashMap<String, PriorityBlockingQueue<WaitSelectedCarBo>> waitSelectedCar;
+    public static LinkedHashMap<String, PriorityBlockingQueue<WaitSelectedCarBo>> waitSelectedCars;
 
     /**
      * key: String= 订单-调度员
+     *
+     * PriorityBlockingQueue<WaitSelectedCarBo>  待调度的车辆 待调度的司机
      */
-    public static LinkedHashMap<String, PriorityBlockingQueue<WaitSelectedDriverBo>> waitSelectedDriver;
+    public static LinkedHashMap<String, PriorityBlockingQueue<WaitSelectedDriverBo>> waitSelectedDrivers;
+
+
 }
