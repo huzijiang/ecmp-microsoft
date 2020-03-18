ce5104dc2f58b668ae1a73b08554db8b93094bb3
shixin_2020 <shixin@hqzhuanche.com>
2020-03-18 21:33:53 +0800

    驾驶员、车辆

---
+++ ms-core/src/main/java/com/hq/ecmp/mscore/mapper/EcmpEnterpriseInfoMapper.java
@@ -0,0 +1,64 @@
+package com.hq.ecmp.mscore.mapper;
+
+import com.hq.ecmp.mscore.domain.EcmpEnterpriseInfo;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 【请填写功能名称】Mapper接口
+ *
+ * @author hqer
+ * @date 2020-01-02
+ */
+@Repository
+public interface EcmpEnterpriseInfoMapper
+{
+    /**
+     * 查询【请填写功能名称】
+     *
+     * @param enterpriseId 【请填写功能名称】ID
+     * @return 【请填写功能名称】
+     */
+    public EcmpEnterpriseInfo selectEcmpEnterpriseInfoById(Long enterpriseId);
+
+    /**
+     * 查询【请填写功能名称】列表
+     *
+     * @param ecmpEnterpriseInfo 【请填写功能名称】
+     * @return 【请填写功能名称】集合
+     */
+    public List<EcmpEnterpriseInfo> selectEcmpEnterpriseInfoList(EcmpEnterpriseInfo ecmpEnterpriseInfo);
+
+    /**
+     * 新增【请填写功能名称】
+     *
+     * @param ecmpEnterpriseInfo 【请填写功能名称】
+     * @return 结果
+     */
+    public int insertEcmpEnterpriseInfo(EcmpEnterpriseInfo ecmpEnterpriseInfo);
+
+    /**
+     * 修改【请填写功能名称】
+     *
+     * @param ecmpEnterpriseInfo 【请填写功能名称】
+     * @return 结果
+     */
+    public int updateEcmpEnterpriseInfo(EcmpEnterpriseInfo ecmpEnterpriseInfo);
+
+    /**
+     * 删除【请填写功能名称】
+     *
+     * @param enterpriseId 【请填写功能名称】ID
+     * @return 结果
+     */
+    public int deleteEcmpEnterpriseInfoById(Long enterpriseId);
+
+    /**
+     * 批量删除【请填写功能名称】
+     *
+     * @param enterpriseIds 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteEcmpEnterpriseInfoByIds(Long[] enterpriseIds);
+}
---
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/EcmpEnterpriseInfoServiceImpl.java
@@ -0,0 +1,96 @@
+package com.hq.ecmp.mscore.service.impl;
+
+import java.util.List;
+
+import com.hq.ecmp.mscore.domain.EcmpEnterpriseInfo;
+
+import com.hq.ecmp.mscore.mapper.EcmpEnterpriseInfoMapper;
+import com.hq.ecmp.mscore.service.IEcmpEnterpriseInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @author hqer
+ * @date 2020-01-02
+ */
+@Service
+public class EcmpEnterpriseInfoServiceImpl implements IEcmpEnterpriseInfoService
+{
+    @Autowired
+    private EcmpEnterpriseInfoMapper ecmpEnterpriseInfoMapper;
+
+    /**
+     * 查询【请填写功能名称】
+     *
+     * @param enterpriseId 【请填写功能名称】ID
+     * @return 【请填写功能名称】
+     */
+    @Override
+    public EcmpEnterpriseInfo selectEcmpEnterpriseInfoById(Long enterpriseId)
+    {
+        return ecmpEnterpriseInfoMapper.selectEcmpEnterpriseInfoById(enterpriseId);
+    }
+
+    /**
+     * 查询【请填写功能名称】列表
+     *
+     * @param ecmpEnterpriseInfo 【请填写功能名称】
+     * @return 【请填写功能名称】
+     */
+    @Override
+    public List<EcmpEnterpriseInfo> selectEcmpEnterpriseInfoList(EcmpEnterpriseInfo ecmpEnterpriseInfo)
+    {
+        return ecmpEnterpriseInfoMapper.selectEcmpEnterpriseInfoList(ecmpEnterpriseInfo);
+    }
+
+    /**
+     * 新增【请填写功能名称】
+     *
+     * @param ecmpEnterpriseInfo 【请填写功能名称】
+     * @return 结果
+     */
+    @Override
+    public int insertEcmpEnterpriseInfo(EcmpEnterpriseInfo ecmpEnterpriseInfo)
+    {
+        return ecmpEnterpriseInfoMapper.insertEcmpEnterpriseInfo(ecmpEnterpriseInfo);
+    }
+
+    /**
+     * 修改【请填写功能名称】
+     *
+     * @param ecmpEnterpriseInfo 【请填写功能名称】
+     * @return 结果
+     */
+    @Override
+    public int updateEcmpEnterpriseInfo(EcmpEnterpriseInfo ecmpEnterpriseInfo)
+    {
+        return ecmpEnterpriseInfoMapper.updateEcmpEnterpriseInfo(ecmpEnterpriseInfo);
+    }
+
+    /**
+     * 批量删除【请填写功能名称】
+     *
+     * @param enterpriseIds 需要删除的【请填写功能名称】ID
+     * @return 结果
+     */
+    @Override
+    public int deleteEcmpEnterpriseInfoByIds(Long[] enterpriseIds)
+    {
+        return ecmpEnterpriseInfoMapper.deleteEcmpEnterpriseInfoByIds(enterpriseIds);
+    }
+
+    /**
+     * 删除【请填写功能名称】信息
+     *
+     * @param enterpriseId 【请填写功能名称】ID
+     * @return 结果
+     */
+    @Override
+    public int deleteEcmpEnterpriseInfoById(Long enterpriseId)
+    {
+        return ecmpEnterpriseInfoMapper.deleteEcmpEnterpriseInfoById(enterpriseId);
+    }
+}
