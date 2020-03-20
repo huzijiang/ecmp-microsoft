86096aefb5f3860dc5e18d129b553265ece84ec8
chenmi <1522472057@qq.com>
2020-03-20 14:47:37 +0800

    commit

--- ms-api/src/main/java/com/hq/ecmp/ms/api/controller/car/DriverInfoController.java
+++ ms-api/src/main/java/com/hq/ecmp/ms/api/controller/car/DriverInfoController.java
@@ -2,6 +2,8 @@ package com.hq.ecmp.ms.api.controller.car;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hq.common.core.api.ApiResponse;
+import com.hq.common.utils.ServletUtils;
+import com.hq.core.security.LoginUser;
+import com.hq.core.security.service.TokenService;
 import com.hq.ecmp.mscore.domain.DriverCreateInfo;
 import com.hq.ecmp.mscore.domain.DriverQuery;
 import com.hq.ecmp.mscore.domain.DriverQueryResult;
@@ -22,11 +27,16 @@ import io.swagger.annotations.ApiOperation;
 public class DriverInfoController {
 		@Autowired
 		IDriverInfoService driverInfoService;
+		@Autowired
+		TokenService tokenService;
 	
 	
 	@ApiOperation(value = "create", notes = "新增驾驶员", httpMethod = "POST")
 	@PostMapping("/create")
 	public ApiResponse create(@RequestBody DriverCreateInfo driverCreateInfo) {
+		 HttpServletRequest request = ServletUtils.getRequest();
+	      LoginUser loginUser = tokenService.getLoginUser(request);
+	      driverCreateInfo.setOptUserId(loginUser.getUser().getUserId());
 		boolean createDriver = driverInfoService.createDriver(driverCreateInfo);
 		if(createDriver){
 			return ApiResponse.success();
