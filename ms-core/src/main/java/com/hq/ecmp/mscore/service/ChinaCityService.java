250ebc5835e70de8b9389273590da626dfb6905e
chenmi <1522472057@qq.com>
2020-03-21 21:35:06 +0800

    城市接口逻辑调整

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/ChinaCityService.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/ChinaCityService.java
@@ -1,5 +1,12 @@
 package com.hq.ecmp.mscore.service;
 
+import java.util.List;
+
+import com.hq.ecmp.mscore.bo.CityInfo;
+
 public interface ChinaCityService {
 	public String queryCityCodeByCityName(String cityName);
+	
+	
+	public List<CityInfo> queryCityInfoListByCityName(String cityName);
 }
