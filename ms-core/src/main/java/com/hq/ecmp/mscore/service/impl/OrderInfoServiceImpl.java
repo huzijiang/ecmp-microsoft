7ed4f7fc473df9dc2944c8a2d19b066e64eacd8c
xueyong <xueyong@01zhuanche.com>
2020-03-23 17:24:16 +0800

    解决BUG ，启动问题

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderInfoServiceImpl.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderInfoServiceImpl.java
@@ -21,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -95,7 +96,8 @@ public class OrderInfoServiceImpl implements IOrderInfoService
     private IDriverHeartbeatInfoService iDriverHeartbeatInfoService;
     @Resource
     private OrderAddressInfoMapper orderAddressInfoMapper;
-    @Resource
+    @Autowired
+    @Lazy
     private IJourneyUserCarPowerService iJourneyUserCarPowerService;
     @Resource
     IRegimeInfoService iRegimeInfoService;
