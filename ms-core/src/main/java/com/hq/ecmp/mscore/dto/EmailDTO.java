e0d61a5d6b2998d194dd291583d21d6a3fec93f8
shixin_2020 <shixin@hqzhuanche.com>
2020-03-24 14:48:42 +0800

    邀请注册代码优化

--- ms-core/src/main/java/com/hq/ecmp/mscore/dto/EmailDTO.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/dto/EmailDTO.java
@@ -1,21 +1,19 @@
 package com.hq.ecmp.mscore.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
-/**
- * @author  shixin
- * @date 2020-03-17
- */
 @Data
+@ApiModel("部门员工dto(返回前端)")
 public class EmailDTO {
-    @ApiParam(name = "Id", value = "用户ID", required = false )
-    private Integer Id;
-    @ApiParam(name = "userId", value = "用户ID", required = true )
+
+    @ApiModelProperty(value = "用户ID")
     private Long userId;
-    @ApiParam(name = "email", value = "邮箱地址", required = true )
+    @ApiModelProperty(value = "邮箱地址")
     private String email;
-    @ApiParam(name = "state", value = "状态", required = false )
+    @ApiModelProperty(value = "状态")
     private String state;
 
 
