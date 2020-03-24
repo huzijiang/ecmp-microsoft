29773b2ab0cdcbb0e04abe81de211044f1c11236
Cairuikang <18335106425@163.com>
2020-03-24 13:24:52 +0800

    循环依赖

--- ms-api/src/main/java/com/hq/ecmp/ms/api/controller/order/DriverOrderController.java
+++ ms-api/src/main/java/com/hq/ecmp/ms/api/controller/order/DriverOrderController.java
@@ -14,6 +14,7 @@ import com.hq.ecmp.mscore.service.IDriverOrderService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,6 +36,7 @@ import java.util.List;
 public class DriverOrderController {
 
     @Resource
+    @Lazy
     IDriverOrderService iDriverOrderService;
 
 
