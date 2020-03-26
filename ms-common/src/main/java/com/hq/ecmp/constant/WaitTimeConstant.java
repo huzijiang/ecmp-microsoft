f2cd84f7ff44b4b9370f2ade23664005b41d800d
ZhangChao <zhangsan@itcast.cn>
2020-03-26 12:43:55 +0800

    提交申请优化

---
+++ ms-common/src/main/java/com/hq/ecmp/constant/WaitTimeConstant.java
@@ -0,0 +1,23 @@
+package com.hq.ecmp.constant;
+
+/**
+ * @Author: chao.zhang
+ * @Date: 2020/3/25 22:46
+ */
+public final class WaitTimeConstant {
+
+    /**
+     * 航班到达后等待多时时间  用车，单位 分钟
+     *
+     * M010  10分钟
+     * M020  20分钟
+     * M030  30分钟
+     * H100   一小时
+     * H130   一个半小时
+     */
+    public static final String WAIT_TEN_MINUTE = "M010";
+    public static final String WAIT_TWENTY_MINUTE = "M020";
+    public static final String WAIT_THIRTY_MINUTE = "M030";
+    public static final String WAIT_ONE_HOUR = "H100";
+    public static final String WAIT_ONE_AND_HALF_HOUR = "H130";
+}
