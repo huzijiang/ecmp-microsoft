de0a88dbadefd4a5de4a6ab3c935d179ec689ff5
chenmi <1522472057@qq.com>
2020-04-02 18:53:22 +0800

    调度 选择自有车后生成行程预估价

--- ms-core/src/main/java/com/hq/ecmp/mscore/domain/CarGroupDriverInfo.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/domain/CarGroupDriverInfo.java
@@ -11,5 +11,10 @@ public class CarGroupDriverInfo {
 	
 	String deptName;//部门名称
 	
+	String deptLeader;//部门负责人
+	
+	Integer deptUserNum;//部门下的用户人数
+	
+	
 	List<DriverQueryResult> driverList;//司机列表
 }
