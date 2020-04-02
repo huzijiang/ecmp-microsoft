18f67432fb115334a872eaa535c2241f5d280bc7
caobj <caobaojun@hqzhuanche.com>
2020-04-02 16:00:21 +0800

    回调接口增加订单正确判断

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderInfoServiceImpl.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderInfoServiceImpl.java
@@ -1936,6 +1936,9 @@ public class OrderInfoServiceImpl implements IOrderInfoService
         String lableState=thirdPartyOrderState.getString("status");
         String json = thirdPartyOrderState.getString("driverInfo");
         OrderInfo orderInfo = orderInfoMapper.selectOrderInfoById(orderNo);
+        if (orderInfo==null){
+            throw new Exception("订单:"+orderNo+"不存在");
+        }
         OrderInfo newOrderInfo = new OrderInfo(orderNo,status);
         if(OrderState.ALREADYSENDING.getState().equals(status)||OrderState.REASSIGNPASS.getState().equals(status)){
             DriverCloudDto driverCloudDto=new DriverCloudDto();
