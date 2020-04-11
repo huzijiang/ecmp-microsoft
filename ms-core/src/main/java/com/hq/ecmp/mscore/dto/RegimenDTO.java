d8927105ca5532585d8202e484a3763d7e93a32c
ZhangChao <zhangsan@itcast.cn>
2020-04-11 15:58:42 +0800

    差旅网约车修改bug

--- ms-core/src/main/java/com/hq/ecmp/mscore/dto/RegimenDTO.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/dto/RegimenDTO.java
@@ -1,5 +1,6 @@
 package com.hq.ecmp.mscore.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -9,5 +10,13 @@ import lombok.Data;
 @Data
 public class RegimenDTO {
 
+    @ApiModelProperty(name = "regimenId",value = "用车制度id",required = true)
     private Long regimenId;
+
+    @ApiModelProperty(name = "type",value = "用车权限类型 \n" +
+            "差旅：\n" +
+            "C001  接机\n" +
+            "C009  送机\n" +
+            "C222  市内用车",required = false)
+    private String type;
 }
