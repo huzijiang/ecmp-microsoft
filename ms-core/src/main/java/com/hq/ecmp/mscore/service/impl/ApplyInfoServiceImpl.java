f01a9189aec9b1bf1364db0ef7c72146e7ddf422
xueyong <xueyong@01zhuanche.com>
2020-04-15 14:57:17 +0800

    增加application-prod 配置,循环依赖lazy

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/ApplyInfoServiceImpl.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/ApplyInfoServiceImpl.java
@@ -27,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
@@ -100,6 +101,7 @@ public class ApplyInfoServiceImpl implements IApplyInfoService
     @Autowired
     private EcmpMessageService ecmpMessageService;
     @Autowired
+    @Lazy
     private IOrderInfoService orderInfoService;
 
     private ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("apply-pool-%d").build();
