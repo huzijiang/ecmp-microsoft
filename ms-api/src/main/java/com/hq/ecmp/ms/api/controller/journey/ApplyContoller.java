8db20ac7628fd8c562029e6782d368dab95753b7
chao.zhang <zhangsan@163.com>
2020-05-13 13:54:39 +0800

    差旅申请提交问题修改

--- ms-api/src/main/java/com/hq/ecmp/ms/api/controller/journey/ApplyContoller.java
+++ ms-api/src/main/java/com/hq/ecmp/ms/api/controller/journey/ApplyContoller.java
@@ -1,5 +1,7 @@
 package com.hq.ecmp.ms.api.controller.journey;
 
+import com.alibaba.fastjson.JSONArray;
+import com.google.gson.JsonObject;
 import com.hq.common.core.api.ApiResponse;
 import com.hq.common.exception.CustomException;
 import com.hq.common.utils.ServletUtils;
@@ -96,10 +98,9 @@ public class ApplyContoller {
         officialCommitApply.setCompanyId(companyId);
         try {
             applyVO = applyInfoService.applyOfficialCommit(officialCommitApply);
-            log.info("公务申请提交参数：{},申请人电话：{}",officialCommitApply,loginUser.getUser().getPhonenumber());
+            log.info("公务申请提交参数：{},申请人电话：{}",JSONArray.toJSON(officialCommitApply).toString(),loginUser.getUser().getPhonenumber());
         } catch (Exception e) {
-            e.printStackTrace();
-            log.error("公务申请提交失败，请求参数：{},申请人电话：{}",officialCommitApply,loginUser.getUser().getPhonenumber());
+            log.error("公务申请提交失败，请求参数：{},申请人电话：{}",JSONArray.toJSON(officialCommitApply).toString(),loginUser.getUser().getPhonenumber(),e);
             return ApiResponse.error("提交公务申请失败");
         }
         return ApiResponse.success("提交申请成功",applyVO);
@@ -123,10 +124,9 @@ public class ApplyContoller {
         travelCommitApply.setCompanyId(companyId);
         try {
             applyVO = applyInfoService.applytravliCommit(travelCommitApply);
-            log.info("差旅申请提交参数：{},申请人电话：{}",travelCommitApply,loginUser.getUser().getPhonenumber());
+            log.info("差旅申请提交参数：{},申请人电话：{}",JSONArray.toJSON(travelCommitApply).toString(),loginUser.getUser().getPhonenumber());
         } catch (Exception e) {
-            e.printStackTrace();
-            log.error("差旅申请提交失败，请求参数：{},申请人电话：{}",travelCommitApply,loginUser.getUser().getPhonenumber());
+            log.error("差旅申请提交失败，请求参数：{},申请人电话：{}",JSONArray.toJSON(travelCommitApply).toString(),loginUser.getUser().getPhonenumber(),e);
             return ApiResponse.error("提交差旅申请失败");
         }
         return ApiResponse.success("提交申请成功",applyVO);
