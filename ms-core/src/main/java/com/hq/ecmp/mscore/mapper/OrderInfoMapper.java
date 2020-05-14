734ecddd7e39dd5595a406bdbe4f07b0f4234f4c
MrLiu <liuzhibin@hqzhuanche.com>
2020-05-14 15:04:55 +0800

    解决错误

--- ms-core/src/main/java/com/hq/ecmp/mscore/mapper/OrderInfoMapper.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/mapper/OrderInfoMapper.java
@@ -9,6 +9,7 @@ import com.hq.ecmp.mscore.dto.OrderListBackDto;
 import com.hq.ecmp.mscore.vo.ApplyDispatchVo;
 import com.hq.ecmp.mscore.vo.DriverOrderInfoVO;
 import com.hq.ecmp.mscore.vo.OrderStateVO;
+import com.hq.ecmp.mscore.vo.RunningOrderVo;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
@@ -279,6 +280,6 @@ public interface OrderInfoMapper {
     //根据订单查询派单耗时
     int getdispatchDurationById(Long orderId);
 
-    List<RunningOrderVo> getRunningOrder(@Param("userId")Long userId,@Param("states")String states);
+    List<RunningOrderVo> getRunningOrder(@Param("userId")Long userId, @Param("states")String states);
 }
 
