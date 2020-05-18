898a022cfc3109c1039f1c2829621532963cea3c
Cairuikang <18335106425@163.com>
2020-05-18 19:04:56 +0800

    解决循环依赖

--- ms-api/src/main/java/com/hq/ecmp/ms/api/controller/pay/WxPayController.java
+++ ms-api/src/main/java/com/hq/ecmp/ms/api/controller/pay/WxPayController.java
@@ -26,6 +26,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -44,6 +45,7 @@ public class WxPayController {
     private WxPayService wxPayService;
 
     @Autowired
+    @Lazy
     private IOrderInfoService iOrderInfoService;
 
     @Autowired
