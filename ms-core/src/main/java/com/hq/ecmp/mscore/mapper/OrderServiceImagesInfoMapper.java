5f337d4aee5f47a8a9a4da281a20e01f09408f9b
MrLiu <liuzhibin@hqzhuanche.com>
2020-05-24 18:04:33 +0800

    图片集合

--- ms-core/src/main/java/com/hq/ecmp/mscore/mapper/OrderServiceImagesInfoMapper.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/mapper/OrderServiceImagesInfoMapper.java
@@ -1,6 +1,17 @@
 package com.hq.ecmp.mscore.mapper;
 
+import com.hq.ecmp.mscore.dto.ordercost.OrderServiceImagesInfo;
+
+import java.util.List;
+
 public interface OrderServiceImagesInfoMapper {
+
+    /***
+     *
+     * @param orderServiceImagesInfo
+     * @return
+     */
+    List<OrderServiceImagesInfo> getList(OrderServiceImagesInfo orderServiceImagesInfo);
 
 
 }
