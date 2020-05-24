6996e24a1e154599d602f597c7e09251794ac91a
MrLiu <liuzhibin@hqzhuanche.com>
2020-05-24 17:16:57 +0800

    订单费用

--- ms-core/src/main/java/com/hq/ecmp/mscore/mapper/OrderServiceCostDetailRecordInfoMapper.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/mapper/OrderServiceCostDetailRecordInfoMapper.java
@@ -1,9 +1,21 @@
 package com.hq.ecmp.mscore.mapper;
 
+import com.hq.ecmp.mscore.dto.ordercost.OrderServiceCostDetailRecordInfo;
+
+import java.util.List;
+
 /***
  * 订单费用明细
  */
 public interface OrderServiceCostDetailRecordInfoMapper {
+
+
+    /***
+     *
+     * @param data
+     * @return
+     */
+    List<OrderServiceCostDetailRecordInfo> getList(OrderServiceCostDetailRecordInfo data);
 
 
 
