97e92ee4a790e151f0dbb8f7b035c978227748e8
Cairuikang <18335106425@163.com>
2020-05-26 19:01:30 +0800

    新增获取半日租和整日租方法

---
+++ ms-common/src/main/java/com/hq/ecmp/util/CommonUtils.java
@@ -0,0 +1,52 @@
+package com.hq.ecmp.util;
+
+import com.hq.common.utils.DateUtils;
+import com.hq.ecmp.constant.CharterTypeEnum;
+
+import java.util.Date;
+
+/**
+ * 业务工具类
+ *
+ * @author crk
+ */
+public class CommonUtils {
+
+    /**
+     * 获取多日组中的当天是否为半日租或者整日租
+     * @param startDate
+     * @param endDate
+     * @param userTime
+     * @return
+     */
+    public static String getCarType(Date startDate, Date endDate, double userTime) {
+        String carType = "";
+        Date nowDate = new Date();
+        //判断用车时间是包含0.5天
+        if (userTime % 1 > 0) {
+            //包含0.5天
+            //判断当天是否为起始时间
+            if (DateUtils.isSameDay(startDate, nowDate)) {
+                if (startDate.compareTo(DateUtils.parseDate(DateUtils.formatDate(startDate, DateUtils.YYYY_MM_DD) + " 12:00:00")) > -1) {
+                    carType = CharterTypeEnum.OVERALL_RENT_TYPE.getKey();
+                } else {
+                    carType = CharterTypeEnum.HALF_DAY_TYPE.getKey();
+                }
+            }else{
+                if (startDate.compareTo(DateUtils.parseDate(DateUtils.formatDate(startDate, DateUtils.YYYY_MM_DD) + " 12:00:00")) > -1) {
+                    carType = CharterTypeEnum.OVERALL_RENT_TYPE.getKey();
+                } else {
+                    if(DateUtils.isSameDay(endDate,nowDate)){
+                        carType = CharterTypeEnum.HALF_DAY_TYPE.getKey();
+                    }
+                }
+            }
+
+        } else {
+            carType = CharterTypeEnum.OVERALL_RENT_TYPE.getKey();
+        }
+
+        return carType;
+    }
+
+}
