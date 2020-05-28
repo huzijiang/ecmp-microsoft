e7ccb8256dea5e927f29f4539b25b360c3d1b4c9
Cairuikang <18335106425@163.com>
2020-05-28 22:52:55 +0800

    解决bug

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderSettlingInfoServiceImpl.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderSettlingInfoServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 import com.alibaba.fastjson.JSON;
 import com.hq.common.utils.DateUtils;
+import com.hq.common.utils.StringUtils;
 import com.hq.ecmp.constant.CharterTypeEnum;
 import com.hq.ecmp.constant.CostConfigModeEnum;
 import com.hq.ecmp.constant.OrderServiceType;
@@ -171,7 +172,7 @@ public class OrderSettlingInfoServiceImpl implements IOrderSettlingInfoService
             lastRecordInfo.setBeyondMileageFee(orderSettlingInfoVo.getOverMileagePrice());
             lastRecordInfo.setBeyondTimeFee(orderSettlingInfoVo.getOvertimeLongPrice());
             i = costDetailRecordInfoMapper.update(lastRecordInfo);
-            if(!orderSettlingInfoVo.getImageUrl().equals(null) && !orderSettlingInfoVo.getImageUrl().equals("")){
+            if(StringUtils.isNotEmpty(orderSettlingInfoVo.getImageUrl())){
                 String [] imageUrl = orderSettlingInfoVo.getImageUrl().split(",");
                 OrderServiceImagesInfo imagesInfo = OrderServiceImagesInfo.builder()
                         .recordId(lastRecordInfo.getRecordId()).build();
