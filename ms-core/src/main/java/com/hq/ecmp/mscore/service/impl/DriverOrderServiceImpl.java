9be055b2b38098526859d6a41cb9fd3bd3310341
Cairuikang <18335106425@163.com>
2020-05-28 22:39:42 +0800

    修改数据类型

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/DriverOrderServiceImpl.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/DriverOrderServiceImpl.java
@@ -243,7 +243,7 @@ public class DriverOrderServiceImpl implements IDriverOrderService {
             Long uid = loginUser.getUser().getUserId();*/
             OrderSettlingInfoVo vo = new OrderSettlingInfoVo();
             vo.setOrderId(orderId);//订单Id
-            vo.setTotalMileage(BigDecimal.valueOf(Long.parseLong(mileage==null?"0":mileage)));//订单总里程
+            vo.setTotalMileage(new BigDecimal(mileage==null?"0":mileage));//订单总里程
             vo.setTotalTime(Integer.parseInt(travelTime==null?"0":travelTime));//订单总时长
 
             iorderSettlingInfoService.addExpenseReport(vo, userId,orderInfoOld.getCompanyId());
