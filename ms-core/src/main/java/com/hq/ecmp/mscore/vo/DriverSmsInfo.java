b4200e8c58b2aaf758c7a3447855355cf860935d
xumingyang <xumingyang@hqzhuanche.com>
2020-06-03 11:23:30 +0800

    格式化 司机结束服务短信中的费用总结10.0000格式化为 10.00

--- ms-core/src/main/java/com/hq/ecmp/mscore/vo/DriverSmsInfo.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/vo/DriverSmsInfo.java
@@ -3,40 +3,39 @@ package com.hq.ecmp.mscore.vo;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.aspectj.weaver.patterns.Declare;
+
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 @Data
 @ApiModel(description = "司机端短信模板")
 public class DriverSmsInfo {
 
-    @ApiModelProperty(name = "startDate",value = "開始時間")
+    @ApiModelProperty(name = "startDate", value = "開始時間")
     private String startDate;
-    @ApiModelProperty(name = "endDate",value = "结束时间")
+    @ApiModelProperty(name = "endDate", value = "结束时间")
     private String endDate;
-    @ApiModelProperty(name = "totalFee",value = "费用结算")
+    @ApiModelProperty(name = "totalFee", value = "费用结算")
     private BigDecimal totalFee;
-    @ApiModelProperty(name = "orderNumber",value = "订单号")
+    @ApiModelProperty(name = "orderNumber", value = "订单号")
     private String orderNumber;
-    @ApiModelProperty(name = "carLicense",value = "车牌号")
+    @ApiModelProperty(name = "carLicense", value = "车牌号")
     private String carLicense;
-    @ApiModelProperty(name = "carType",value = "车类型")
+    @ApiModelProperty(name = "carType", value = "车类型")
     private String carType;
-    @ApiModelProperty(name = "driverName",value = "司机名")
+    @ApiModelProperty(name = "driverName", value = "司机名")
     private String driverName;
-    @ApiModelProperty(name = "mobile",value = "司机电话")
+    @ApiModelProperty(name = "mobile", value = "司机电话")
     private String mobile;
-    @ApiModelProperty(name = "carGroupName",value = "车队名")
+    @ApiModelProperty(name = "carGroupName", value = "车队名")
     private String carGroupName;
-    @ApiModelProperty(name = "telephone",value = "车队电话")
+    @ApiModelProperty(name = "telephone", value = "车队电话")
     private String telephone;
-    @ApiModelProperty(name = "dispatcherNickName",value = "司机名称")
+    @ApiModelProperty(name = "dispatcherNickName", value = "司机名称")
     private String dispatcherNickName;
-    @ApiModelProperty(name = "dispatcherPhoneNumber",value = "司机电话")
+    @ApiModelProperty(name = "dispatcherPhoneNumber", value = "司机电话")
     private String dispatcherPhoneNumber;
     private String riderMobile;
-    private String  applyMobile;
+    private String applyMobile;
 
 }
