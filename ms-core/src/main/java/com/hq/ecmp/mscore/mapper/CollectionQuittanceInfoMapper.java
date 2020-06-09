14ca0a84bfe3e0b0bf77530adcc2e1cb9d992d34
mrliu <liuzhibin@hqzhuanche.com>
2020-06-09 18:05:28 +0800

    mappr查询

--- ms-core/src/main/java/com/hq/ecmp/mscore/mapper/CollectionQuittanceInfoMapper.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/mapper/CollectionQuittanceInfoMapper.java
@@ -2,7 +2,10 @@ package com.hq.ecmp.mscore.mapper;
 
 import com.hq.ecmp.mscore.domain.ReckoningInfo;
 import com.hq.ecmp.mscore.dto.ReckoningDto;
+import com.hq.ecmp.mscore.dto.lease.LeaseSettlementDto;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CollectionQuittanceInfoMapper {
@@ -10,4 +13,20 @@ public interface CollectionQuittanceInfoMapper {
     void add(ReckoningInfo param);
 
     void updateReckoningStatus(ReckoningDto param);
+
+
+    /***
+     * 普通用户的租赁列表
+     * @param data
+     * @return
+     */
+    List<LeaseSettlementDto> getOrdinaryUserList(LeaseSettlementDto data);
+
+    /***
+     * add by liuzb
+     * 确认费用
+     * @param data
+     * @return
+     */
+    int ordinaryUserConfirmCost(LeaseSettlementDto data);
 }
