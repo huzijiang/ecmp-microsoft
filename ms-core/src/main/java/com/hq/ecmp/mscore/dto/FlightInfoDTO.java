fd0388a017f41a0969969b32a49abf389663dfc1
shuxiaoxiao <shuxiaoxiao@hqzhuanche.com>
2020-06-11 16:31:57 +0800

    bs端直接调用云端或第三方服务迁移至微服务

--- ms-core/src/main/java/com/hq/ecmp/mscore/dto/FlightInfoDTO.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/dto/FlightInfoDTO.java
@@ -1,8 +1,18 @@
 package com.hq.ecmp.mscore.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 /**
  * @author shuxiao
  * @date 2020/6/10
  */
+@Data
+@ApiModel(description = "航班服务模型")
 public class FlightInfoDTO {
+	@ApiModelProperty(name = "flightCode",value = "航班号")
+	private String flightCode;
+	@ApiModelProperty(name = "flightDate",value = "航班时间")
+	private String flightDate;
 }
