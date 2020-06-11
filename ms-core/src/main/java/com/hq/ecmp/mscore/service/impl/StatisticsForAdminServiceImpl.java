5430ff6ce5809f2863db2910af2e1afa02088558
guhb <195807716@qq.com>
2020-06-11 11:47:53 +0800

    佛山2.0 统计分析增加companyId控制

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/StatisticsForAdminServiceImpl.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/StatisticsForAdminServiceImpl.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.hq.common.core.api.ApiResponse;
+import com.hq.common.utils.ServletUtils;
+import com.hq.core.security.service.TokenService;
 import com.hq.ecmp.mscore.dto.statistics.StatisticsForAdmin;
 import com.hq.ecmp.mscore.mapper.StatisticsForAdminMapper;
 import com.hq.ecmp.mscore.service.StatisticsForAdminService;
@@ -31,11 +33,16 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     private static final Logger log = LoggerFactory.getLogger(StatisticsForAdminServiceImpl.class);
 
     @Autowired
+    private TokenService tokenService;
+
+    @Autowired
     private StatisticsForAdminMapper statisticsForAdminMapper;
 
     //出车次数排行
     @Override
     public ApiResponse driverOutranking(StatisticsForAdmin statisticsForAdmin) {
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         statisticsForAdmin.setBeginDate(statisticsForAdmin.getBeginDate().substring(0, 10)+" 00:00:00");
@@ -65,6 +72,8 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     //用车费用排行
     @Override
     public ApiResponse vehicleExpenses(StatisticsForAdmin statisticsForAdmin) {
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         statisticsForAdmin.setBeginDate(statisticsForAdmin.getBeginDate().substring(0, 10)+" 00:00:00");
@@ -93,7 +102,8 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     //单位用车统计
     @Override
     public ApiResponse unitVehicle(StatisticsForAdmin statisticsForAdmin) {
-
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         PageHelper.startPage(statisticsForAdmin.getPageNum(),statisticsForAdmin.getPageSize());
@@ -134,7 +144,8 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     //机关车辆使用统计
     @Override
     public ApiResponse useOfMechanismVehicles(StatisticsForAdmin statisticsForAdmin) {
-
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         try {
@@ -159,7 +170,8 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     //驾驶员出车统计
     @Override
     public ApiResponse driverOut(StatisticsForAdmin statisticsForAdmin) {
-
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         PageHelper.startPage(statisticsForAdmin.getPageNum(),statisticsForAdmin.getPageSize());
@@ -190,7 +202,8 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     //车型使用统计
     @Override
     public ApiResponse modelUse(StatisticsForAdmin statisticsForAdmin) {
-
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         PageHelper.startPage(statisticsForAdmin.getPageNum(),statisticsForAdmin.getPageSize());
@@ -215,7 +228,8 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     //租赁情况统计
     @Override
     public ApiResponse leasing(StatisticsForAdmin statisticsForAdmin) {
-
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         try {
@@ -239,7 +253,8 @@ public class StatisticsForAdminServiceImpl implements StatisticsForAdminService 
     //详情统计
     @Override
     public ApiResponse details(StatisticsForAdmin statisticsForAdmin) {
-
+        Long companyId = tokenService.getLoginUser(ServletUtils.getRequest()).getUser().getOwnerCompany();
+        statisticsForAdmin.setCompanyId(companyId);
         ApiResponse apiResponse = new ApiResponse();
         JSONObject jsonObject = new JSONObject();
         PageHelper.startPage(statisticsForAdmin.getPageNum(),statisticsForAdmin.getPageSize());
