d21eb8140fe0671380cdc4e306bd7e28cb9b515f
yangnan <yangnan@01zhuanche.com>
2020-06-29 13:58:42 +0800

    修改日志格式

--- ms-common/src/main/java/com/hq/ecmp/constant/InterceptorConstant.java
+++ ms-common/src/main/java/com/hq/ecmp/constant/InterceptorConstant.java
@@ -4,7 +4,7 @@ package com.hq.ecmp.constant;
  * @author yangnan
  */
 public final class InterceptorConstant {
-    public final static String NGINX = "/nginx.html";
+    public final static String NGINX = "/check.html";
 
     /**header拦截器不需要拦截的请求*/
     public final static String[] EXCLUDE_HEADER = {};
