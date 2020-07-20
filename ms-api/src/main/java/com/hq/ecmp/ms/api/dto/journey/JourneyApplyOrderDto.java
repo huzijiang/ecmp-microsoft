3bf3b12278290bbb3c86557deb11fe82da16ac28
huzj <huzijiang@hqzhuanche.com>
2020-07-20 10:50:04 +0800

    撤销申请单操作添加新接口

---
+++ ms-api/src/main/java/com/hq/ecmp/ms/api/dto/journey/JourneyApplyOrderDto.java
@@ -0,0 +1,37 @@
+package com.hq.ecmp.ms.api.dto.journey;
+
+import io.swagger.annotations.ApiParam;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @auther: zj.hu
+ * @date: 2020-07-20
+ */
+@Data
+public class JourneyApplyOrderDto {
+    /**
+     * 申请单编号
+     */
+    @ApiParam(required = false)
+    @NotEmpty
+    @NotNull
+    private Long applyId;
+    /**
+     * 行程单编号
+     */
+    @ApiParam(required = false)
+    @NotEmpty
+    @NotNull
+    private Long jouneyId;
+
+    /**
+     * 订单编号
+     */
+    @ApiParam(required = true)
+    @NotEmpty
+    @NotNull
+    private Long orderNumber;
+}
