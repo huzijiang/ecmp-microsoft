6bf71c249ac357ee913e6d63a27f59e560cc5e9a
lenny <liuliangsailor@163.co>
2020-09-09 14:10:32 +0800

    合并

--- ms-api/src/main/java/com/hq/ecmp/ms/api/controller/statistics/UseCarStatisticsExportController.java
+++ ms-api/src/main/java/com/hq/ecmp/ms/api/controller/statistics/UseCarStatisticsExportController.java
@@ -1,25 +1,16 @@
 package com.hq.ecmp.ms.api.controller.statistics;
 
 import com.alibaba.fastjson.JSONObject;
-import com.hq.api.monitor.domain.SysOperLog;
-import com.hq.api.monitor.domain.vo.SysOperLogVo;
 import com.hq.common.core.api.ApiResponse;
-import com.hq.common.utils.ServletUtils;
-import com.hq.common.utils.poi.ExcelUtil;
 import com.hq.core.aspectj.lang.annotation.Log;
 import com.hq.core.aspectj.lang.enums.BusinessType;
-import com.hq.core.security.LoginUser;
 import com.hq.core.security.service.TokenService;
-import com.hq.core.web.domain.AjaxResult;
 import com.hq.ecmp.mscore.service.UseCarSumService;
-import com.hq.ecmp.mscore.vo.ApplyDispatchVo;
-import com.hq.ecmp.mscore.vo.PageResult;
 import com.hq.ecmp.vo.UseCarSumExportVo;
 import com.hq.ecmp.vo.UseCarSumVo;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
