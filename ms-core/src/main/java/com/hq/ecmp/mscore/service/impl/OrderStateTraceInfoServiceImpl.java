a31f110577689b00492a890742cbfa7465ad268b
lenny <liuliangsailor@163.co>
2020-09-24 19:25:01 +0800

    导出修改637

--- ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderStateTraceInfoServiceImpl.java
+++ ms-core/src/main/java/com/hq/ecmp/mscore/service/impl/OrderStateTraceInfoServiceImpl.java
@@ -339,30 +339,32 @@ public class OrderStateTraceInfoServiceImpl implements IOrderStateTraceInfoServi
 	@Override
 	public List<StatisticsForAdminDetailVo> userDeptUseCarDetailExport(UserDeptUseCarDetailDto userDeptUseCarDetailDto, LoginUser loginUser) {
 		//用户部门
-		Long deptId = loginUser.getUser().getDept().getDeptId();
+		Long deptId = 491l;
 		Integer pageNum = userDeptUseCarDetailDto.getPageNum();
 		Integer pageSize = userDeptUseCarDetailDto.getPageSize();
 		String beginDate = userDeptUseCarDetailDto.getBeginDate();
 		String endDate = userDeptUseCarDetailDto.getEndDate();
 		String carGroupName = userDeptUseCarDetailDto.getCarGroupName();
-		if(pageNum == null){
-			pageNum = 1;
-		}
-		if(pageSize == null){
-			pageSize = 100;
-		}
-		PageHelper.startPage(pageNum,pageSize);
-		boolean isgo = true;
 		List<StatisticsForAdminDetailVo> list  = orderInfoMapper.userDeptUseCarDetail(beginDate,endDate,carGroupName,deptId);
-		while (isgo){
-			pageNum = pageNum+1;
-			PageHelper.startPage(pageNum,pageSize);
-			List<StatisticsForAdminDetailVo> tmp = orderInfoMapper.userDeptUseCarDetail(beginDate,endDate,carGroupName,deptId);
-			if(tmp == null || tmp.size()==0){
-				isgo = false;
-			}
-			list.addAll(tmp);
-		}
+
+//		if(pageNum == null){
+//			pageNum = 1;
+//		}
+//		if(pageSize == null){
+//			pageSize = 100;
+//		}
+//		PageHelper.startPage(pageNum,pageSize);
+//		boolean isgo = true;
+//		List<StatisticsForAdminDetailVo> list  = orderInfoMapper.userDeptUseCarDetail(beginDate,endDate,carGroupName,deptId);
+//		while (isgo){
+//			pageNum = pageNum+1;
+//			PageHelper.startPage(pageNum,pageSize);
+//			List<StatisticsForAdminDetailVo> tmp = orderInfoMapper.userDeptUseCarDetail(beginDate,endDate,carGroupName,deptId);
+//			if(tmp == null || tmp.size()==0){
+//				isgo = false;
+//			}
+//			list.addAll(tmp);
+//		}
 		return list;
 	}
 }
